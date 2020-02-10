66427e5ec81010ebff287598c447cb16bb2750ab
Marcia Corte <cortemarcia80@gmail.com>
2020-02-10 17:22:02 -0300

    Create File UserService

--- Avanade.AvaSpace/AvaSpace.Domain/Services/UserService.cs
+++ Avanade.AvaSpace/AvaSpace.Domain/Services/UserService.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using AvaSpace.Domain.Entities;
+using AvaSpace.Domain.Interfaces.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AvaSpace.Domain.Services
 {
-    class UserService
+    public class UserService : ServiceBase<User>, IUserService
     {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository) : base(userRepository)
+        {
+            _userRepository = userRepository;
+        }
     }
 }
