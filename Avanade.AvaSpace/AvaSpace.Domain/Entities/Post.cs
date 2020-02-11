8cda7a16843661dc2aea34618ec8c7e8d8bea2e4
Amanda Andrade <andr.mandy@gmail.com>
2020-02-11 20:20:33 +0000

    aprimoramento das validações da entidade

--- Avanade.AvaSpace/AvaSpace.Domain/Entities/Post.cs
+++ Avanade.AvaSpace/AvaSpace.Domain/Entities/Post.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AvaSpace.Domain.Entities
+namespace Gama.RedeSocial.Domain.Entities
 {
     public class Post : BaseEntity
     {
         public Post()
         {
             Midias = new List<Midia>();
-
             Likes = new List<Like>();
-
             Comments = new List<Post>();
-        }   
-        
-        public Guid AuthorId { get; set; }
-        public User Author { get; set; }
+            Author = new User();
+        }
+
         public Guid ParentId { get; set; }
-        public string Text  { get; set; }
+        public string Text { get; set; }
         public List<Midia> Midias { get; set; }
         public List<Like> Likes { get; set; }
         public List<Post> Comments { get; set; }
 
+        public Guid AuthorId { get; set; }
+        public User Author { get; set; }
+
         public override void Validate()
         {
-            throw new NotImplementedException();
+            if(AuthorId == Guid.Empty)
+                throw new ArgumentNullException("'AuthorId' não foi preenchido");
+
         }
+
     }
+    
 }
