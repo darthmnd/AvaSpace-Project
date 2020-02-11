e0b8df7e9fc66d1568ade157d27ff0364e50f3b0
Marcia Corte <cortemarcia80@gmail.com>
2020-02-11 13:52:55 -0300

    Change structure File Gender.cs

--- Avanade.AvaSpace/AvaSpace.Domain/Entities/Gender.cs
+++ Avanade.AvaSpace/AvaSpace.Domain/Entities/Gender.cs
@@ -4,6 +4,13 @@ namespace AvaSpace.Domain.Entities
 {
     public class Gender : BaseEntity
     {
+        private string v;
+
+        public Gender(string v)
+        {
+            this.v = v;
+        }
+
         public string Description { get; set; }
 
         public override void Validate()
