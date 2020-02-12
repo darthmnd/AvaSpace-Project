27eaa1891cfb48ecc2da4b12c998bd6c17d24d83
Marcia <cortemarcia80@gmail.com>
2020-02-12 16:34:52 -0300

    Correct error on these files

--- Avanade.AvaSpace/AvaSpace.Domain/Entities/Gallery.cs
+++ Avanade.AvaSpace/AvaSpace.Domain/Entities/Gallery.cs
@@ -15,6 +15,7 @@ namespace AvaSpace.Domain.Entities
 
         public Guid AuthorId { get; set; }
         public User Author { get; set; }
+        public Guid UserId { get; set; }
 
         public override void Validate()
         {
