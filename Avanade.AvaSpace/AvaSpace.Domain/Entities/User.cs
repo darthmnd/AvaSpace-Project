a3ada9f631b21c1231634206cddccb79b2880df2
Marcia <cortemarcia80@gmail.com>
2020-02-12 13:09:27 -0300

    Remove the cover on validate

--- Avanade.AvaSpace/AvaSpace.Domain/Entities/User.cs
+++ Avanade.AvaSpace/AvaSpace.Domain/Entities/User.cs
@@ -45,9 +45,12 @@ namespace AvaSpace.Domain.Entities
 
             if (Birthday == DateTime.MinValue)
                 throw new ArgumentException("Data de Nascimento é inválido");
-
+            
             if (GenderId == Guid.Empty)
-                throw new ArgumentNullException("O gênero é obrigatório");
+                throw new ArgumentNullException("'GenderId' não foi preenchido");
+            
+            if (AvatarId == Guid.Empty)
+                throw new ArgumentNullException("'AvatarId' não foi preenchido");
         }
     }
 }
