26312e5626c9bb42b0640e40bc2632d4d783af30
Amanda Andrade <andr.mandy@gmail.com>
2020-02-12 14:57:05 -0300

    Add Post and Like Repositories

---
+++ Avanade.AvaSpace/AvaSpace.Repository/Repositories/PostRespository.cs
@@ -0,0 +1,14 @@
+﻿using Gama.RedeSocial.Domain.Entities;
+using Gama.RedeSocial.Domain.Interfaces.Repositories;
+using System;
+
+namespace Gama.RedeSocial.Infrastructure.Persistence.Repository.Repositories
+{
+    public class PostRespository : RepositoryBase<Post>, IPostRepository
+    {
+        public override Post Get(Guid id)
+        {
+            
+        }
+    }
+}
